322cc80c1b257e93541362c0d47926c78212634d
radiegtya <radiegtya@yahoo.co.id>
2018-03-16 11:09:02 +0700

    searching

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from ..models import Courses
@@ -8,4 +9,9 @@ class CoursesViewSet(viewsets.ModelViewSet):
 
     queryset = Courses.objects.all()
     serializer_class = CoursesSerializer
+    filter_backends = (
+        DjangoFilterBackend,
+        SearchFilter,
+    )
     filter_fields = ('title', 'price',)
+    search_fields = ('title', 'subtitle', 'price',)
