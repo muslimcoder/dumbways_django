b88a404dd9c8efe88c6f1a6909ba00d68a50ddcc
radiegtya <radiegtya@yahoo.co.id>
2018-03-18 06:42:52 +0700

    drf post one to many

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework import status
 
 from ..models import Courses
 from ..seriazilers import CoursesSerializer
@@ -15,3 +17,9 @@ class CoursesViewSet(viewsets.ModelViewSet):
     )
     filter_fields = ('title', 'price',)
     search_fields = ('title', 'subtitle', 'price',)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
